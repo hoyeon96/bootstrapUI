41da378e8ed3b43316e332869da5eb1193c58e2e
hoyeon96 <hoyen96@naver.com>
2021-05-22 05:15:54 +0900

    최소크기 깨짐현상 수정

--- js/toggle.js
+++ js/toggle.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function (window, document) {
   'use strict';
 
@@ -26,33 +25,4 @@
   //     toggle.classList.remove('on');
   //   });
   // }
-=======
-(function (window, document) {
-  'use strict';
-
-  const $toggles = document.querySelectorAll('.toggle'); // Return NodeList
-  const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
-  
-  $toggleBtn.addEventListener('click', function () {
-    toggleElements();
-  });
-
-  // window.addEventListener('resize', function () {
-  //   if (window.innerWidth > 768) {
-  //     offElements();
-  //   }
-  // });
-
-  function toggleElements() {
-    [].forEach.call($toggles, function (toggle) {
-      toggle.classList.toggle('on');
-    });
-  }
-
-  // function offElements() {
-  //   [].forEach.call($toggleBtn, function (toggleBtn) {
-  //     toggle.classList.remove('on');
-  //   });
-  // }
->>>>>>> 7c3b6bb7abcb2684d269bb12fbe1ef020cdc1346
 })(window, document);
