078333dcff8b4a2c979263c2b4006989361b0132
hoyeon96 <hoyen96@naver.com>
2021-05-22 17:28:05 +0900

    누르는 상태에서 toggle border 있게 만들기

--- js/togglebtn.js
+++ js/togglebtn.js
@@ -6,6 +6,43 @@
   const $btnclick = document.querySelectorAll('.btnclick');
 
 
+  $toggleBtn.addEventListener('mousedown', function () {
+  toggleElements2();
+  });
+
+
+
+   $toggleBtn.addEventListener('click', function () {
+  toggleElements2();
+  });
+
+
+  function toggleElements2() {
+  [].forEach.call($btnclick, function (toggle) {
+  	
+  	if(!$('.btnclick').hasClass('clicked')){
+	    toggle.classList.toggle('clicked');
+  		}
+  	
+  });
+}
+
+  })(window, document);
+
+$(document).click(function(e){ 
+	if (!$(e.target).is('.toggle')) {
+	 	$(".btnclick.clicked").classList.remove("clicked");
+	 } 
+	 //ex
+	//   if (!$(e.target).is('#')) {
+	//    $("#해당영역ID").css("display","none"); 
+	// } 
+});
+
+
+
+
+
 // 태훈이 코드 //
   // $toggleBtn.addEventListener('click', function () {
   // toggleElements();
@@ -16,19 +53,3 @@
 //       toggle.classList.toggle('on');
 //     });
 // }
-
-
-  $toggleBtn.addEventListener('click', function () {
-  toggleElements2();
-  });
-
-  function toggleElements2() {
-  [].forEach.call($btnclick, function (toggle) {
-    toggle.classList.toggle('clicked');
-    toggle.classList.toggle('visible');
-  });
-}
-
-  })(window, document);
-
-
