f46d12cc71007d4f63c44af99528d8fb325067e8
hoyeon96 <hoyen96@naver.com>
2021-05-22 13:54:25 +0900

    토글 버튼 작동 및 slide 처리

--- js/toggle.js
+++ js/toggle.js
@@ -1,28 +1,30 @@
-(function (window, document) {
-  'use strict';
+// (function (window, document) {
+//   'use strict';
 
-  const $toggles = document.querySelectorAll('.toggle'); // Return NodeList
-  const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
-  
-  $toggleBtn.addEventListener('click', function () {
-    toggleElements();
+//   const $toggles = document.querySelectorAll('.toggle-menu'); // Return NodeList
+//   const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
+
+//   $toggleBtn.addEventListener('click', function () {
+//     toggleElements();
+//   });
+
+//   function toggleElements() {
+//     [].forEach.call($toggles, function (toggle) {
+//       toggle.classList.toggle('on');
+//     });
+
+//       // toggle.classList.slidetoggle(1300,'on',function(){
+//       //   alert("슬라이드 토글 되었습니다.");
+//       // });
+//   }
+
+
+// })(window, document);
+
+
+
+$(function(){
+  $("#toggle-btn").click(function(){
+     $("#toggle-menu").slideToggle("slow");
   });
-
-  // window.addEventListener('resize', function () {
-  //   if (window.innerWidth > 768) {
-  //     offElements();
-  //   }
-  // });
-
-  function toggleElements() {
-    [].forEach.call($toggles, function (toggle) {
-      toggle.classList.toggle('on');
-    });
-  }
-
-  // function offElements() {
-  //   [].forEach.call($toggleBtn, function (toggleBtn) {
-  //     toggle.classList.remove('on');
-  //   });
-  // }
-})(window, document);
+});
