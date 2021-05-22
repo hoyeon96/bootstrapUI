e113c21fe1dc3cf8945a676ea3b3795971fbac32
hoyeon96 <hoyen96@naver.com>
2021-05-22 15:15:57 +0900

    토글 버튼 작동 수정

---
+++ js/togglebtn.js
@@ -0,0 +1,33 @@
+(function (window, document) {
+  'use strict';
+
+  const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
+  const $toggles = document.querySelectorAll('.toggle'); // Return NodeList
+  const $btnclick = document.querySelectorAll('.btnclick');
+
+
+// 태훈이 코드 //
+  // $toggleBtn.addEventListener('click', function () {
+  // toggleElements();
+  // });
+
+//   function toggleElements() {
+//     [].forEach.call($toggles, function (toggle) {
+//       toggle.classList.toggle('on');
+//     });
+// }
+
+
+  $toggleBtn.addEventListener('click', function () {
+  toggleElements2();
+  });
+
+  function toggleElements2() {
+  [].forEach.call($btnclick, function (toggle) {
+    toggle.classList.toggle('clicked');
+  });
+}
+
+  })(window, document);
+
+
